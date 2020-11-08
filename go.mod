cadedf275db12b6175122d76679a2d3f5d7bbec0
Usmon <usmon1983@gmail.com>
2020-11-09 00:51:58 +0500

    create server.go file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/usmon1983/http
+
+go 1.15
---
+++ pkg/server/server.go
@@ -0,0 +1,113 @@
+package server
+
+import (
+	"strings"
+	"bytes"
+	"io"
+	"log"
+	"sync"
+	"net"
+)
+
+type HandlerFunc func (conn net.Conn)
+
+type Server struct {
+	addr string
+	mu sync.RWMutex
+	handlers map[string]HandlerFunc
+}
+
+func NewServer(addr string) *Server  {
+	return &Server{addr: addr, handlers: make(map[string]HandlerFunc)}
+}
+
+func (s *Server) Register(path string, handler HandlerFunc)  {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.handlers[path] = handler
+}
+
+func (s *Server) Start() error  {
+	listener, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	defer func ()  {
+		if cerr := listener.Close(); cerr != nil {
+			if err == nil {
+				err = cerr
+				return
+			}
+			log.Print(cerr)
+		}
+	}()		
+
+	for {
+	conn, err := listener.Accept()
+		
+		if err != nil {
+			log.Print(err)
+			//идем обслуживать следующего
+			continue
+		}
+		go s.handle(conn)
+				
+	}
+}
+
+func (s *Server) handle(conn net.Conn) (err error) {
+	defer func() {
+		if cerr := conn.Close(); cerr != nil {
+			if err == nil {
+				err = cerr
+				return
+			}
+			log.Print(err)
+		}
+	}()
+
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err == io.EOF {
+		log.Printf("%s", buf[:n])
+		return nil
+	}
+	if err != nil {
+			return err
+	}
+
+	data := buf[:n]
+	requestLineDelim := []byte{'\r', '\n'}
+	requestLineEnd := bytes.Index(data, requestLineDelim)
+	if requestLineEnd == -1 {
+		log.Printf("%s", buf[:n])
+	}
+
+	requestLine := string(data[:requestLineEnd])
+	parts := strings.Split(requestLine, " ")
+	if len(parts) != 3 {
+		return err
+	}
+
+	path, version := parts[1], parts[2]
+
+	if version != "HTTP/1.1" {
+		return err
+	}
+
+	var handler = func(conn net.Conn) {
+		conn.Close()
+	}
+	s.mu.RLock()
+	for i := 0; i < len(s.handlers); i++ {
+		if hr, find := s.handlers[path]; find {
+			handler = hr
+			break
+		}
+	}
+	s.mu.RUnlock()
+	handler(conn)
+
+	return nil
+}
