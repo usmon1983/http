1e984357c2932b11162de2e117b39f8a642b50b5
UMuhitdinov <usmon1983@gmail.com>
2020-11-16 15:23:14 +0500

    some changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"github.com/usmon1983/http/pkg/server"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"github.com/usmon1983/http/pkg/banners"
+	"github.com/usmon1983/http/cmd/app"
 	"net"
 	"os"
 )
@@ -16,19 +19,43 @@ func main() {
 	}
 }
 
-func execute(host string, port string) (err error)  {
-	srv := server.NewServer(net.JoinHostPort(host, port))
-	srv.Register("/", func (req *server.Request)  {
-		id := req.QueryParams["id"]
-		log.Println(id)
-	})
-	srv.Register("/payments/{id}", func (req *server.Request)  {
-		id := req.PathParams["id"]
-		log.Print(id)
-	})
-	srv.Register("/payments/{id}", func (req *server.Request)  {
-		id := req.Headers["id"]
-		log.Print(id)
-	})
-	return srv.Start()
+func execute(host string, port string) (err error) {
+	mux := http.NewServeMux()
+	bannersSvc := banners.NewService()
+	server := app.NewServer(mux, bannersSvc)
+	
+	server.Init()
+	srv := &http.Server{
+		Addr: net.JoinHostPort(host, port),
+		Handler: http.HandlerFunc(func (writer http.ResponseWriter, request *http.Request)  {
+			log.Print(request.RequestURI) //полный URI
+			log.Print(request.Method) //метод
+			//log.Print(request.Header) //все заголовки
+			//log.Print(request.Header.Get("Content-Type")) //конкретный заголовок
+
+			//log.Print(request.FormValue("tags")) //только первое значение Query + POST
+			//log.Print(request.PostFormValue("tags")) //только первое значение POST
+
+			body, err := ioutil.ReadAll(request.Body) //тело запроса
+			if err != nil {
+				log.Print(err)
+			}
+			log.Print("%s", body)
+
+			err = request.ParseMultipartForm(10 * 1024 * 1024) //10Mb
+			if err != nil {
+				log.Print(err)
+			}
+			
+			//доступно только после ParseForm (либо FormValue, PostFormValue)
+			//log.Print(request.Form)
+			//log.Print(request.PostForm)
+			//доступно только после ParseMultipart (FormValue, PostFormValue автоматически вызывают ParseMultipartForm)
+			//log.Print(request.FormFile("image"))
+			//request.MultipartForm.Value - только "обычные поля"
+			//request.MultipartForm.File - только файлы
+			
+		}),
+	}
+	return srv.ListenAndServe()
 }
---
+++ pkg/banners/service.go
@@ -0,0 +1,87 @@
+package banners
+
+import (
+	"mime/multipart"
+	"log"
+	"errors"
+	"sync"
+	"context"
+	
+)
+
+//Service представляет собой сервис по управлению баннерами.
+type Service struct {
+	mu sync.RWMutex
+	items []*Banner
+}
+
+//NewService создаёт сервис.
+func NewService() *Service  {
+	return &Service{items: make([]*Banner, 0)}
+}
+
+//Banner представляет собой баннер.
+type Banner struct {
+	ID int64
+	Title string
+	Content string
+	Button string
+	Link string
+	Image string
+}
+
+//All возвращает все существующие баннеры.
+func (s *Service) All(ctx context.Context) ([]*Banner, error)  {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.items, nil
+}
+
+//ByID возвращает баннер по идентификатору.
+func (s *Service) ByID(ctx context.Context, id int64) (*Banner, error)  {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	for _, v := range s.items {
+		if v.ID == id {
+			return v, nil
+		}
+	}
+
+	return nil, errors.New("item not found")
+}
+
+var BannerID int64 = 0
+//Save сохраняет/обновляет баннер.
+func (s *Service) Save(ctx context.Context, item *Banner, file multipart.File) (*Banner, error)  {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	log.Print("ddddd")
+	if item.ID == 0 {
+		BannerID++
+		item.ID = BannerID
+		s.items = append(s.items, item)
+		return item, nil
+	}
+	for k, v := range s.items {
+		if v.ID == item.ID {
+			s.items[k] = item
+			return item, nil
+		}
+	}
+	return nil, errors.New("item not found")
+}
+
+//RemoveByID удаляет баннер по идентификатору.
+func (s *Service) RemoveByID(ctx context.Context, id int64) (*Banner, error)  {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	for k, v := range s.items {
+		if v.ID == id {
+			s.items = append(s.items[:k], s.items[k+1:]...)
+			return v, nil
+		}
+	}
+
+	return nil, errors.New("item not found")
+}
